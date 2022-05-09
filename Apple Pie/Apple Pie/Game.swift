34e3c525045e63673641a58611a413332516e604
Capslock <99589445+Capslockhuh@users.noreply.github.com>
2022-05-09 20:15:00 +0200

    Add files via upload

---
+++ Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,21 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Jan Andrzejewski on 09/05/2022.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
